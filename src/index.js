877bb2bbef086e5e287b888e8b3ac35059e98a0a
Muny1re1 <munyiriandy@gmail.com>
2024-04-06 14:32:12 +0300

    Fetch on forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (e)=> {
+        e.preventDefault();
+        const input = document.querySelector("input#searchByID");
+        console.log(input.value);
+        fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const title = document.querySelectpr("section#movieDetails h4")
+        const summary = document.querySelectpr("section#movieDetails p")
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+});
+};
 
 document.addEventListener('DOMContentLoaded', init);
